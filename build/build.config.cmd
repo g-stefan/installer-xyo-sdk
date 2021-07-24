f4fa5ac4075b669b69511cdc900bc9bdae1a8ee9
Grigore Stefan <g_stefan@yahoo.com>
2021-07-24 23:00:25 +0300

    update

--- build/build.config.cmd
+++ build/build.config.cmd
@@ -4,5 +4,5 @@ rem http://unlicense.org/
 rem Created by Grigore Stefan <g_stefan@yahoo.com>
 
 set PRODUCT_NAME=installer-xyo-sdk
-set PRODUCT_VERSION=4.1.0
+set PRODUCT_VERSION=4.2.0
 set PRODUCT_BASE=xyo-sdk
